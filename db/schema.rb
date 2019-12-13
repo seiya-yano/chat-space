ed84d6e4ff73a7f44b9b57105796903a50c9555b
seiya-yano <seyaseyaseiya@gmail.com>
2019-12-13 15:57:59 +0900

    グループ管理完成

--- db/schema.rb
+++ db/schema.rb
@@ -22,8 +22,10 @@ ActiveRecord::Schema.define(version: 20191209123011) do
   end
 
   create_table "groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name",       null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_groups_on_name", unique: true, using: :btree
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
