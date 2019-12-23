fbf193542b0810890b4d48a525334402fb761c73
seiya-yano <seyaseyaseiya@gmail.com>
2019-12-23 21:44:12 +0900

    もうちょい

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,5 +1,4 @@
 $(function(){
-  console.log(last_message_id);
   var buildHTML = function(message) {
     image = (message.image) ? `<img class= "lower-message__image" src=${message.image} >` : "";
     var html = `<div class="message" data-message-id="${message.id}"> 
@@ -20,48 +19,40 @@ $(function(){
         </div>`
     return html;
   }
-  }
-  $('#new_message').on('submit', function(e){
-    e.preventDefault()
-    var formData = new FormData(this);
-    var url = $(this).attr('action')
+  var reloadMessages = function() {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    var last_message_id = $('.message:last').data("message-id");
+  console.log(last_message_id)
     $.ajax({
-      url: url,
-      type: 'POST',
-      data: formData, 
+      //ルーティングで設定した通りのURLを指定
+      url: "api/messages",
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'get',
       dataType: 'json',
-      processData: false,
-      contentType: false
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
     })
-    .done(function(data){
-      var html = buildHTML(data);
-      $('.messages').append(html);
-      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
-      $('form')[0].reset();
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        //追加するHTMLの入れ物を作る
+        var insertHTML = '';
+        //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        //メッセージが入ったHTMLに、入れ物ごと追加
+        $('.messages').append(insertHTML);
+        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+        $("#new_message")[0].reset();
+        $(".form__submit").prop("disabled", false);
+      }
     })
-    .fail(function(){
-      alert('error');
+    .fail(function() {
+      console.log('error');
     });
-    return false;
-  });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
+  // console.log(last_message_id);
 });
-//     $.ajax({
-//       url: url,
-//       type: 'POST',
-//       data: formData, 
-//       dataType: 'json',
-//       processData: false,
-//       contentType: false
-//     })
-//     .done(function(data){
-//       var html = buildHTML(data);
-//       $('.messages').append(html);
-//       $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
-//       $('form')[0].reset();
-//     })
-//     .fail(function(){
-//       alert('error');
-//     });
-//     return false;
-//   });
-// });
